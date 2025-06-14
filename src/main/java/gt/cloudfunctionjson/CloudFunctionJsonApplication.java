ce7d6e473d2c9c15b9188ef6515a98e8db818e8b
Ganesh Tiwari <ganesh.tiwari@shutterfly.com>
2025-06-13 21:14:11 -0500

    init

---
+++ src/main/java/gt/cloudfunctionjson/CloudFunctionJsonApplication.java
@@ -0,0 +1,22 @@
+package gt.cloudfunctionjson;
+
+import com.google.gson.Gson;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.function.json.GsonMapper;
+import org.springframework.cloud.function.json.JsonMapper;
+import org.springframework.context.annotation.Bean;
+
+@SpringBootApplication
+public class CloudFunctionJsonApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CloudFunctionJsonApplication.class, args);
+    }
+
+
+    @Bean
+    JsonMapper gsonMapper(Gson gson) {
+        return new GsonMapper(gson);
+    }
+}
